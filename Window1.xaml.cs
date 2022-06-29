f131799d785a295abf6a9e1fcaa21398fc471d51
johnnasser13 <108411565+johnnasser13@users.noreply.github.com>
2022-06-29 15:44:36 +0200

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+namespace DB_II
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-95V0MFCD;Initial Catalog=pharmacy;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter adap;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textDCode.Text == "" || textDName.Text == "" || textPay.Text == "")
+            {
+                MessageBox.Show("please enter full details");
+            }
+            else
+            {
+               
+
+                    cmd = new SqlCommand("Insert into product values('" + textDCode.Text + "','" + textDName.Text + "','" + textPay.Text + "')", con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Record has been Saved succesfully");
+                
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            cmd = new SqlCommand("Update product set price='" + textPay.Text + "' where id='" + textDCode.Text + "'", con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+            MessageBox.Show("Record has been Updated succesfully");
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            cmd = new SqlCommand("DELETE product WHERE id = '" + textDCode.Text + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+
+            MessageBox.Show("Record has been Deleted succesfully");
+            
+        }
+    }
+}
---
+++ login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DB_II
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            if (textbox1.Text == "john" && passwordBox.Password == "123")
+            {
+                Window1 w = new Window1();
+                w.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show(" Enter Correct Data ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        
+    }
+}
